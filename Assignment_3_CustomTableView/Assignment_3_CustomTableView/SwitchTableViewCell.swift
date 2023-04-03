04abb9d5fd36605729da48e10c7eb382e31d11cb
KetulPatel14 <128503538+KetulPatel14@users.noreply.github.com>
2023-04-03 12:30:10 -0400

    add Switch

---
+++ Assignment_3_CustomTableView/Assignment_3_CustomTableView/SwitchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchTableViewCell.swift
+//  Assignment_3_CustomTableView
+//
+//  Created by Canadore Student on 2023-04-03.
+//  Copyright © 2023 Canadore Student. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
